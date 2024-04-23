b6e3ea5e52270f166ed7e872e4205f1ef4452dec
Elsayed155 <e7936493@gmail.com>
2024-04-23 20:31:02 +0200

    e-commerce

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "../../node_modules/@fortawesome/fontawesome-free/css/all.min.css"
+// import "../../node_modules/@fortawesome/fontawesome-free/css/all.min.css"
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
 import './index.css';
