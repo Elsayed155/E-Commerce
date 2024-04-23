9efb57fce952c52f43fc64e6fdffc6e4d54a2d17
Elsayed155 <e7936493@gmail.com>
2024-04-23 20:48:10 +0200

    e-commerce

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import "../../node_modules/@fortawesome/fontawesome-free/css/all.min.css"
-// import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
-// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
+ import "@fortawesome/fontawesome-free/css/all.min.css"
+ import "bootstrap/dist/css/bootstrap.min.css"
 import './index.css';
 import TokenContextProvider from './Context/Token';
 import "slick-carousel/slick/slick.css";
